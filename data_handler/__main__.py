120a96f094b5cfa8bbe674dc779e7772006163ce
Vicgothicoi <3109716510@qq.com>
2026-07-11 21:04:13 +0800

    Deploy

--- data_handler/__main__.py
+++
@@ -1,4 +0,0 @@
-from data_handler.cli import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
--- data_handler/export/__init__.py
+++
@@ -1,4 +0,0 @@
-from data_handler.export.graph_exporter import GraphExporter, GraphExportResult
-from data_handler.export.id_allocator import IdAllocator
-
-__all__ = ["IdAllocator", "GraphExporter", "GraphExportResult"]
